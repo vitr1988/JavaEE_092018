dd1ce3f48ee975f4848fda865f1f80a1010197ad
Vitalii Ivanov <vitr.ivanov@gmail.com>
2018-10-08 15:42:39 +0400

    feat (JavaEE_092018): Изменения по сервлету NashornServlet

--- src/main/java/ru/otus/web/NashornServlet.java
+++ src/main/java/ru/otus/web/NashornServlet.java
@@ -2,8 +2,7 @@ package ru.otus.web;
 
 import jdk.nashorn.api.scripting.NashornScriptEngineFactory;
 
-import javax.script.ScriptEngine;
-import javax.script.ScriptException;
+import javax.script.*;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -16,17 +15,31 @@ import java.io.PrintWriter;
 public class NashornServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         NashornScriptEngineFactory factory = new NashornScriptEngineFactory();
+//        ScriptEngine engine = new ScriptEngineManager().getEngineByName("nashorn"); // the same
         ScriptEngine engine = factory.getScriptEngine(new String[] { "-scripting" });
 
+        Bindings bindings = engine.createBindings();
+        bindings.put("count", 3);
+        bindings.put("name", "Vitalii");
+
         try(PrintWriter pw = response.getWriter()){
-            pw.println(engine.eval(request.getParameter("text")));
+            String programForExecution = request.getParameter("text");
+            pw.println(engine.eval(programForExecution, bindings));
+            Invocable invocable = (Invocable) engine;
+            engine.eval("function composeGreeting(name) {" +
+                "return 'Hello ' + name" +
+            "}");
+            pw.println(invocable.invokeFunction("composeGreeting", "Vitalii"));
         }
         catch(ScriptException e){
             e.printStackTrace();
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
         }
+        //var greeting='hello world';print(greeting);greeting
+        //readLine("What is your name? ")
         //$EXEC("notepad")
         //$ENV.PWD
-        /* var name = readLine("What is your name? ")
-        print("Hello, ${name}!")*/
+
     }
 }
