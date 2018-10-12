a9532d077053ac28932cc1d21df6afa447853d6c
ViTr <ViTr@list.ru>
2018-10-12 21:51:27 +0400

    feat (JavaEE): Поддежка Server PUSH

---
+++ src/main/java/ru/otus/servlet/PushBuilderServlet.java
@@ -0,0 +1,24 @@
+package ru.otus.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.*;
+import java.io.IOException;
+
+@WebServlet("/push")
+public class PushBuilderServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        final PushBuilder pushBuilder = request.newPushBuilder(); // local variables are thread safe
+        if (pushBuilder != null) {
+            pushBuilder.path("images/news.jpg")
+                        .addHeader("content-type", "image/jpg")
+                    .push();
+            pushBuilder.path("css/style-all.min.css")
+                        .addHeader("content-type", "text/css")
+                    .push();
+        }
+        request.getRequestDispatcher("/html/page.html").forward(request, response);
+    }
+}
