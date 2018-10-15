97fcf61efe8adc03249b926e2a24a0cc0b231fa9
ViTr <ViTr@list.ru>
2018-10-15 23:05:00 +0400

    feat (JavaEE): Работа с сервлетами

--- src/main/java/ru/otus/servlet/MultithreadServlet.java
+++ src/main/java/ru/otus/servlet/MultithreadServlet.java
@@ -2,6 +2,7 @@ package ru.otus.servlet;
 
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
+import javax.servlet.SingleThreadModel;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
--- src/main/java/ru/otus/servlet/ServletForTestingPurpose.java
+++ src/main/java/ru/otus/servlet/ServletForTestingPurpose.java
@@ -1,20 +1,87 @@
 package ru.otus.servlet;
 
+import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.*;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.stream.Stream;
 
-@WebServlet(name = "ServletForTestingPurpose", urlPatterns = "/testing")
+@WebServlet(name = "ServletForTestingPurpose", urlPatterns = {"/testing", "/test/*", "*.proto"})
 public class ServletForTestingPurpose extends HttpServlet {
+
+    @Override
+    public void init(ServletConfig config) throws ServletException {
+        super.init(config);
+    }
+
+    @Override
+    public void init() throws ServletException {
+
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        HttpServletRequestWrapper requestWrapper = new HttpServletRequestWrapper(request){
+            @Override
+            public String getParameter(String name) {
+                String value = super.getParameter(name);
+                if (name.equalsIgnoreCase("url")) {
+                    value = "http://" + value;
+                }
+                return value;
+            }
+        };
+        request.getRequestDispatcher("/push").forward(requestWrapper, response);
+
         response.setContentType("text/html");
         try(PrintWriter pw = response.getWriter()) {
             pw.print("Hello World");
+        }
+    }
+
+    private int index = 1;
+
+    @Override
+    protected void doPut(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        index++;
+        String url = request.getParameter("url");
+        HttpSession session = request.getSession(false);
+        if (session == null && !session.isNew()) {
+            session = request.getSession();
+        }
+        session.setAttribute("", "");
+        Cookie[] cookies = request.getCookies();
+        if (cookies != null) {
+            Stream.of(cookies).forEach(c -> {
+                String cookieName = c.getName();
+                String cookieValue = c.getValue();
+            });
+
+        }
+        response.setStatus(HttpServletResponse.SC_CREATED);
+        response.getWriter().println("Hello World");
+        Cookie cookie = new Cookie("customCoookie", 1 + "");
+        response.addCookie(cookie);
+
+        getServletContext().setAttribute("", "");
+    }
+
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        switch(req.getMethod().toLowerCase()) {
+            case "get" : {
+                doGet(req, resp); break;
+            }
+            case "post" : {
+                doPost(req, resp); break;
+            }
         }
     }
 }
