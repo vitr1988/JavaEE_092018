56b9297552799b1696b4adf03a3b57af51cc286f
Vitalii Ivanov <vitr.ivanov@gmail.com>
2018-11-12 16:47:49 +0400

    feat (JavaEE_092018): работа с JSF

---
+++ src/main/java/ru/otus/jsf/bean/UserNumberBean.java
@@ -0,0 +1,37 @@
+package ru.otus.jsf.bean;
+
+import lombok.Data;
+
+import javax.enterprise.context.SessionScoped;
+import javax.inject.Named;
+import java.io.Serializable;
+import java.util.Random;
+
+@Data
+@Named
+@SessionScoped
+public class UserNumberBean implements Serializable {
+
+    private static final long serialVersionUID = 5443351151396868724L;
+
+    Integer randomInt;
+    private Integer userNumber;
+    String response;
+    private int maximum = 10;
+    private int minimum = 0;
+
+    public UserNumberBean() {
+        Random randomGR = new Random();
+        randomInt = new Integer(randomGR.nextInt(maximum + 1));
+        // Print number to server log
+        System.out.println("Duke's number: " + randomInt);
+    }
+
+    public String getResponse() {
+        if ((userNumber == null) || (userNumber.compareTo(randomInt) != 0)) {
+            return "Sorry, " + userNumber + " is incorrect.";
+        } else {
+            return "Yay! You got it!";
+        }
+    }
+}
