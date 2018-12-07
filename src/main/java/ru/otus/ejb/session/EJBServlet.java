9271ee57952c610b8532c356b8c29b2a1a8ed94c
ViTr <ViTr@list.ru>
2018-12-07 22:17:33 +0400

    feat (JavaEE): EJB support

--- src/main/java/ru/otus/ejb/session/EJBServlet.java
+++ src/main/java/ru/otus/ejb/session/EJBServlet.java
@@ -1,5 +1,6 @@
 package ru.otus.ejb.session;
 
+import ru.otus.ejb.session.singleton.SingletonBean;
 import ru.otus.ejb.session.stateful.AnotherSimpleBean;
 import ru.otus.ejb.session.stateless.ApplicationSimpleBean;
 
@@ -10,6 +11,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Objects;
 
 @WebServlet("/ejb")
 public class EJBServlet extends HttpServlet {
@@ -20,10 +23,17 @@ public class EJBServlet extends HttpServlet {
     @EJB
     AnotherSimpleBean anotherBean;
 
+    @EJB
+    SingletonBean singletonBean;
+
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         bean.doSomething();
-        response.getWriter().println(bean.saySmth());
-        response.getWriter().println(anotherBean.incrementLastValue(25));
+        PrintWriter writer = response.getWriter();
+        writer.println(bean.saySmth());
+        writer.println(anotherBean.incrementLastValue(25));
+        writer.println(Objects.toString(singletonBean.get(Long.MAX_VALUE), "Nothing in cache"));
+        singletonBean.put(Long.MAX_VALUE, "Something is in cache");
+
     }
 }
--- src/test/java/ru/otus/ejb/EJBClientTest.java
+++ src/test/java/ru/otus/ejb/EJBClientTest.java
@@ -3,6 +3,7 @@ package ru.otus.ejb;
 import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.Test;
+import ru.otus.ejb.session.singleton.SingletonBean;
 import ru.otus.ejb.session.stateful.AnotherSimpleBean;
 import ru.otus.ejb.session.stateless.ApplicationSimpleBean;
 
@@ -11,16 +12,18 @@ import javax.naming.NamingException;
 import java.io.IOException;
 import java.util.Properties;
 
-public class JNDIClientTest {
+public class EJBClientTest {
 
-    private static InitialContext ctx;
+    private static InitialContext ctxByPropertyFile;
+    private static InitialContext defaultCtx;
 
     @BeforeClass
     public static void setup(){
         Properties props = new Properties();
         try {
-            props.load(JNDIClientTest.class.getResourceAsStream("/jndi.properties"));
-            ctx = new InitialContext(props);
+            props.load(EJBClientTest.class.getResourceAsStream("/jndi.properties"));
+            ctxByPropertyFile = new InitialContext(props);
+            defaultCtx = new InitialContext();
         } catch (IOException | NamingException ex) {
             ex.printStackTrace();
         }
@@ -29,22 +32,31 @@ public class JNDIClientTest {
     @Test
     public void testSimpleBeanInsideGlassfish() throws Exception {
         // glassfish specific test case (non-portable JNDI name)
-        ApplicationSimpleBean bean = (ApplicationSimpleBean) ctx.lookup("ejb/SimpleBean");
+        ApplicationSimpleBean bean = (ApplicationSimpleBean) ctxByPropertyFile.lookup("ejb/SimpleBean");
         Assert.assertEquals("Hello world", bean.saySmth());
     }
 
     @Test
     public void testSimpleBeanGlobally() throws Exception {
         // universal test case (portable JNDI name)
-        ApplicationSimpleBean bean = (ApplicationSimpleBean) new InitialContext().lookup("java:global/JavaEE/SimpleBean!ru.otus.ejb.session.stateless.ApplicationSimpleBean");
+        ApplicationSimpleBean bean = (ApplicationSimpleBean) defaultCtx.lookup("java:global/JavaEE/SimpleBean!ru.otus.ejb.session.stateless.ApplicationSimpleBean");
         Assert.assertEquals("Hello world", bean.saySmth());
     }
 
     @Test
     public void testAnotherSimpleBeanGlobally() throws Exception {
-        AnotherSimpleBean bean = (AnotherSimpleBean) new InitialContext().lookup("java:global/JavaEE/AnotherSimpleBeanImpl!ru.otus.ejb.session.stateful.AnotherSimpleBean");
+        AnotherSimpleBean bean = (AnotherSimpleBean) defaultCtx.lookup("java:global/JavaEE/AnotherSimpleBeanImpl!ru.otus.ejb.session.stateful.AnotherSimpleBean");
         int lastResult = 15;
         Assert.assertEquals(lastResult, bean.incrementLastValue(lastResult));
         Assert.assertEquals(2 * lastResult, bean.incrementLastValue(lastResult));
+    }
+
+    @Test
+    public void testSingletonBean() throws Exception {
+        SingletonBean bean = (SingletonBean) defaultCtx.lookup("java:global/JavaEE/SingletonBeanImpl!ru.otus.ejb.session.singleton.SingletonBean");
+        Long key = Long.MAX_VALUE;
+        Assert.assertNull(bean.get(key));
+        bean.put(key, key + "");
+        Assert.assertNotNull(bean.get(key));
     }
 }
